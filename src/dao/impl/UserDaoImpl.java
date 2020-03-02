3320d72c0af0db37f79083fe483d4cb822809658
果 <@LAPTOP-L9H4OEOH>
2020-03-02 16:55:15 +0800

    我的第二次提交

--- src/dao/impl/UserDaoImpl.java
+++ src/dao/impl/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package dao.impl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import dao.UserDao;
@@ -11,17 +12,27 @@ import enity.User;
  *
  */
 public class UserDaoImpl implements UserDao {
-
+	List<User> list = new ArrayList<User>();
 	@Override
 	public void save(User user) {
 		// 创建一个集合来保存用户信息
-		List<User> list = new ArrayList<User>();
 		list.add(user);
 		for(User user2 : list) {
 			System.out.println(user2.getName()+","
 					+ ""+user2.getAge());
 		}
 
+	}
+
+	@Override
+	public void update(User user) {
+		System.out.println("执行修改用户的操作");
+		for(User user1 : list) {
+			System.out.println(user1.getName()+","
+					+ ""+user1.getAge());
+		}
+		System.out.println("测试");
+		
 	}
 
 }
